50bedc7e61213bf8b3eb3a503b8690217701cd44
Abimbola Oladele <31113550+consoledot@users.noreply.github.com>
2024-08-07 04:34:28 +0100

    add database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +17,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -29,11 +35,22 @@ func main() {
 	// default to 4000 and development of no corresponding flag provided
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+
+	flag.StringVar(&cfg.db.dsn, "ds-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgresSQL DSN")
 	flag.Parse()
 
 	// initialize logger
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal((err))
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	// Application instance
 
@@ -41,8 +58,6 @@ func main() {
 		config: cfg,
 		logger: logger,
 	}
-
-
 
 	// Declear http server
 
@@ -56,8 +71,26 @@ func main() {
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 
 	fmt.Println("Hello world")
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
